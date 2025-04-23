be9c24c74516d3a8f66f0fe409c4b5b9e14c82a6
detectiv876vh <vha7244@gmail.com>
2025-04-24 00:16:30 +0700

    pha dc breakable tiles o ca 2 map

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -57,10 +57,17 @@ public class TileManager {
         int row = worldY / gp.tileSize;
 
         if (col >= 0 && col < gp.maxWorldCol && row >= 0 && row < gp.maxWorldRow) {
-            int tileIndex = mapTileNum[1][col][row];
+            int tileIndex = mapTileNum[gp.currentMap][col][row];
 
-            if (tile[tileIndex].breakable) {
-                mapTileNum[1][col][row] = 4; // Gán về tile nền
+            if(gp.currentMap == 0) {
+                if (tile[tileIndex].breakable) {
+                    mapTileNum[0][col][row] = 0; // Gán về tile nền
+                }
+            }
+            else if (gp.currentMap == 1) {
+                if (tile[tileIndex].breakable) {
+                    mapTileNum[1][col][row] = 4; // Gán về tile nền
+                }
             }
         }
     }
